93e83b8fd6a417da02d19fb15b024331a143ae82
rovshanguli <rovshanshg@code.edu.az>
2021-12-20 01:30:29 +0400

    Methods

---
+++ Methods/Methods/Models/MyList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods.Models
+{
+    class MyList<T>
+    {
+        #region AddMethod
+        static int counter = -1;
+        public T[] arr;
+
+        public MyList()
+        {
+            arr = new T[0];
+
+        }
+        public void Add(T param)
+        {
+            counter++;
+            if (counter % 10 == 0)
+            {
+                Array.Resize<T>(ref arr, arr.Length + 10);
+            }
+
+
+            arr[counter] = param;
+
+        }
+        public void GetAll()
+        {
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void Length()
+        {
+            Console.WriteLine(arr.Length);
+        }
+        #endregion
+        #region AddRangeMethod
+        //public void AddRange(int[] arr)
+        //{
+        //    int[] rangearr = new int[arr.Length];
+        //    for (int i = 0; i < arr.Length; i++)
+        //    {
+        //        rangearr[i] = arr[i];
+        //    }
+        //    foreach (var item in rangearr)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+        //}
+        #endregion
+        #region FindMethod
+       
+        #endregion
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,42 @@
+﻿using Methods.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Add
+            //MyList<int> list = new MyList<int>();
+            //list.Add(5);
+            //list.Add(12);
+            //list.Add(23);
+            //list.Add(1);
+            //list.Add(65);
+            //list.Add(78);
+            //list.Add(65);
+            //list.Add(32);
+            //list.Add(85);
+            //list.Add(19);
+            //list.Add(42);
+            //list.Add(85);
+            //list.GetAll();
+            //list.Length();
+            #endregion
+            
+            #region AddRangeMethod
+            //int[] intArray = { 1, 5, 9, 6, 8, 12,17};
+            //MyList<int> list = new MyList<int>();
+            //list.AddRange(intArray);
+            #endregion
+
+
+            #region FinfMethod
+
+            #endregion
+        }
+
+    }
+}
